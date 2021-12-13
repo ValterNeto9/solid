52ff77ff7202bf00f63a148be32c00f50d7e68ec
Valter Ferreira Lopes Neto <valter.neto@datum.inf.br>
2021-12-13 15:55:41 -0300

    Criando o projeto, configurações do tsconfig.

---
+++ src/app.ts
@@ -0,0 +1,4 @@
+import { CarrinhoCompras } from "./srp/carrinho_compras/carrinho-compras"
+
+const carrinhoCompras = new CarrinhoCompras()
+carrinhoCompras.teste()
---
+++ src/srp/carrinho_compras/carrinho-compras.ts
@@ -0,0 +1,5 @@
+export class CarrinhoCompras {
+    teste(){
+        console.log('Testando!...')
+    }
+}
